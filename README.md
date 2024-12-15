5e35c418db0d015edc8ff99214197e03169d566c
jack9761 <jackmietz@gmail.com>
2024-12-14 17:19:13 -0800

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# MercedPublicTransit
+# Merced Public Transit
+This repository contains code to calculate statistics about Merced's public transit system. The main product of this is a ArcGis Pro toolkit (MercedPublicTransitToolkit.pyt). 
+
+Right now this project is incomplete, the project likely will not work without modification. The project will be updated to improve functionality and fix bugs. 
+
+To install, first create a conda environment based on environment.yml
+Next you can open the toolkit in ArcGis Pro. Right now only "fastest mode" is implemented.
+The road network, graphml, and feature databases are provided as feathes, use those to prevent having to recompute them. 
+Make sure to select a building that would be in a database when selecting the point of interest.
+The output is a geojson and feather file on your E drive. You should change this to a better location. 
+You can then import the geojson file into ArcGIS. 
---
+++ utils.py
@@ -0,0 +1,45 @@
+﻿import osmnx
+import geopandas
+class Stop:
+    def __init__(self, OSM_ID: str, times: [int]):
+        self.OSM_ID = OSM_ID
+        self.times = times
+class Route:
+   def __init__(self, operatingdays: str, route_name: str, operater: str, stops: [Stop]):
+       self.operatingdays = operatingdays
+       self.route_name = route_name
+       self.operater = operater
+       self.stops = stops
+
+def compute_travel_time(graph, route):
+    return osmnx.routing.route_to_gdf(graph, route)["travel_time"].sum()
+def compute_travel_time_for_index_orgin(index, busdatabase, route, time_of_day, destination_node, gdfclean):
+    return CalculateTravelTime(busdatabase, route, time_of_day, destination_node, gdfclean.loc[gdfclean.index[index], "element"][0] + str(gdfclean.loc[gdfclean.index[index], "id"]))
+def compute_travel_time_for_index_destination(index, busdatabase, route, time_of_day, origin_node, gdfclean):
+    return CalculateTravelTime(busdatabase, route, time_of_day, gdfclean.loc[gdfclean.index[index], "element"][0] + str(gdfclean.loc[gdfclean.index[index], "id"]), origin_node)
+def CalculateTravelTime(busdatabase: geopandas.GeoDataFrame, currentroute: Route, time_of_day: float,
+                        orginnode: str, destinationnode: str):
+    besttraveltime =10000000000
+    traveltime = 0
+    for stopa in currentroute.stops:
+        orginstop = stopa
+        bestdeparturetime = 0
+        for time in stopa.times:
+            if time_of_day+(busdatabase.loc[(orginnode[0], int(orginnode[1:])), orginstop.OSM_ID]/60) < time:
+                bestdeparturetime = time
+                break
+        for stopb in currentroute.stops:
+            deststop = stopb
+            bestarrivaltime = 0
+            for time in stopb.times:
+                if bestdeparturetime < time:
+                    bestarrivaltime = time
+                    break
+            walktofirststoptime = busdatabase.loc[(orginnode[0], int(orginnode[1:])), orginstop.OSM_ID] / 60
+            waitforbustime = bestdeparturetime - time_of_day - walktofirststoptime
+            onthebustime = bestarrivaltime - bestdeparturetime
+            walktodestinationtime = busdatabase.loc[(destinationnode[0], int(destinationnode[1:])), deststop.OSM_ID] / 60
+            traveltime = walktofirststoptime+ waitforbustime + onthebustime + walktodestinationtime
+            if traveltime < besttraveltime:
+                besttraveltime = traveltime
+    return besttraveltime
